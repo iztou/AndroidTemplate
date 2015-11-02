aef9ed60e0964be509fc65545dd942b76e69f1df
ml3515582 <asd166132@gmail.com>
2015-11-02 14:51:27 +0800

    增加pageView滑动指示用例

---
+++ app/src/main/java/gml/template/androidtemplate/pagerslidingtab/PageSlidingTabActivity.java
@@ -0,0 +1,15 @@
+package gml.template.androidtemplate.pagerslidingtab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import gml.template.androidtemplate.R;
+
+public class PageSlidingTabActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_sliding_tab_acticity);
+    }
+}
