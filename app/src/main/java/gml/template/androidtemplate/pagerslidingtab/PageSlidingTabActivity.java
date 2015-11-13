f61f30d7ac0294b81bb1bf9de18b06b2fe6800f4
ml3515582 <asd166132@gmail.com>
2015-11-13 18:49:31 +0800

    增加获取location代码

--- app/src/main/java/gml/template/androidtemplate/pagerslidingtab/PageSlidingTabActivity.java
+++ app/src/main/java/gml/template/androidtemplate/pagerslidingtab/PageSlidingTabActivity.java
@@ -2,10 +2,14 @@ package gml.template.androidtemplate.pagerslidingtab;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.text.SpannableStringBuilder;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,16 +26,20 @@ import gml.template.androidtemplate.MyApplication;
 import gml.template.androidtemplate.R;
 import gml.template.androidtemplate.util.ColorPhrase;
 
-public class PageSlidingTabActivity extends Activity {
+public class PageSlidingTabActivity extends Activity implements LocationListener {
 
     private PagerSlidingTabStrip strip;
     private ViewPager viewPager;
+    private double latitude;
+    private double longitude;
+    private double altitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_sliding_tab_acticity);
         initView();
+        startLocationService();
     }
 
     private void initView() {
@@ -40,6 +48,44 @@ public class PageSlidingTabActivity extends Activity {
         SimplePagerAdapter simplePagerAdapter = new SimplePagerAdapter();
         viewPager.setAdapter(simplePagerAdapter);
         strip.setViewPager(viewPager);
+    }
+
+    /**
+     * 获取位置信息服务
+     */
+    private void startLocationService() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, this);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        latitude = location.getLatitude();     //经度
+        longitude = location.getLongitude(); //纬度
+        altitude =  location.getAltitude();     //海拔
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        //当坐标改变时触发此函数，如果Provider传进相同的坐标，它就不会被触发
+        // log it when the location changes
+        if (location != null) {
+            Log.i("SuperMap", "Location changed : Lat: "
+                    + location.getLatitude() + " Lng: "
+                    + location.getLongitude());
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 
     class SimplePagerAdapter extends PagerAdapter {
@@ -70,6 +116,11 @@ public class PageSlidingTabActivity extends Activity {
                     .put("first_name", "颜龙")
                     .put("age", "19")
                     .format();
+            CharSequence location = Phrase.from("经度：{latitude} \n 维度：{longitude} \n 海拔:{altitude}")
+                    .put("latitude", String.valueOf(latitude))
+                    .put("longitude", String.valueOf(longitude))
+                    .put("altitude", String.valueOf(altitude))
+                    .format();
             List<String> formatString = new ArrayList<>();
             formatString.add("1");
             formatString.add("2");
@@ -78,7 +129,7 @@ public class PageSlidingTabActivity extends Activity {
             CharSequence chars = ColorPhrase.from("ColorPhrase字符颜色替换处理：\n I'm {Chinese},I love {China} \n").innerColor(0xFFE6454A).outerColor(0xFF666666).format();
             if (position == 0) {
                 textView.setText(chars);
-                textView.append(formatted + "\n" + result + "\n");
+                textView.append(formatted + "\n" + result + "\n" + location);
             } else {
                 textView.setText("第" + ++position + "页");
             }
