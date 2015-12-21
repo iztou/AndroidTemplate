2d193bd7c64b7b7a80b6f8e8bb0ebc5c544a8afb
ml3515582 <asd166132@gmail.com>
2015-12-21 11:46:15 +0800

    greendao 研究

---
+++ app/greenDao/src/gml/greendao/TableGenerator.java
@@ -0,0 +1,25 @@
+package gml.greendao;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Schema;
+
+/**
+ * Created by user on 2015/12/21.
+ */
+public class TableGenerator {
+    public static void main(String args[]) throws Exception {
+        Schema schema = new Schema(1000, "gml.greendao");
+        addUser(schema);
+        new DaoGenerator().generateAll(schema, "../src/main/java");
+    }
+
+    private static void addUser(Schema schema){
+        Entity user = schema.addEntity("User");
+        user.addIdProperty();
+        user.addDateProperty("username").notNull();    // 开始时间
+        user.addDateProperty("password");      // 结束时间
+        user.addLongProperty("createtime");      // 循环间隔时间
+        user.addStringProperty("remark");    // 通知内容
+    }
+}
