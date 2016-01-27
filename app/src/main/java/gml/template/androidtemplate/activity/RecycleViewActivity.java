097dd0ac5286bd14268aaaaa2ba32c2ae25834d6
ml3515582 <asd166132@gmail.com>
2016-01-27 18:45:28 +0800

    add itemDecoration

--- app/src/main/java/gml/template/androidtemplate/activity/RecycleViewActivity.java
+++ app/src/main/java/gml/template/androidtemplate/activity/RecycleViewActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.List;
 import butterknife.Bind;
 import gml.template.androidtemplate.R;
 import gml.template.androidtemplate.adapter.RecyclerViewAdapter;
+import gml.template.androidtemplate.view.DividerItemDecoration;
 
 public class RecycleViewActivity extends BaseActivity {
 
@@ -29,27 +32,38 @@ public class RecycleViewActivity extends BaseActivity {
 
     private void fillData(){
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
         viewAdapter = new RecyclerViewAdapter(this);
         recyclerView.setAdapter(viewAdapter);
         viewAdapter.setData(getInstalledApps(true));
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                Log.d("测试RecyclerView滚动距离","x滚动距离=>"+dx+",y滚动距离=>"+dy);
+            }
+        });
     }
 
 
     private ArrayList<PInfo> getInstalledApps(boolean getSysPackages) {
         ArrayList<PInfo> res = new ArrayList<PInfo>();
-        List<PackageInfo> packs = getPackageManager().getInstalledPackages(0);
+        List<PackageInfo> packs = getApplicationContext().getPackageManager().getInstalledPackages(0);
         for (int i = 0; i < packs.size(); i++) {
             PackageInfo p = packs.get(i);
             if ((!getSysPackages) && (p.versionName == null)) {
                 continue;
             }
             PInfo newInfo = new PInfo();
-            newInfo.appname = p.applicationInfo.loadLabel(getPackageManager())
+            newInfo.appname = p.applicationInfo.loadLabel(getApplicationContext().getPackageManager())
                     .toString();
             newInfo.pname = p.packageName;
             newInfo.versionName = p.versionName;
             newInfo.versionCode = p.versionCode;
-            newInfo.icon = p.applicationInfo.loadIcon(getPackageManager());
+            newInfo.icon = p.applicationInfo.loadIcon(getApplicationContext().getPackageManager());
             res.add(newInfo);
         }
         return res;
