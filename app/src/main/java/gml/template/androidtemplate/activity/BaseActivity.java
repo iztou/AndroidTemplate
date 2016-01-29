ec2b515f73ff23135a34ff03da7f982fc12df682
ml3515582 <asd166132@gmail.com>
2016-01-29 11:59:02 +0800

    handle home icon

--- app/src/main/java/gml/template/androidtemplate/activity/BaseActivity.java
+++ app/src/main/java/gml/template/androidtemplate/activity/BaseActivity.java
@@ -77,10 +77,11 @@ public class BaseActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+        }else if(id == android.R.id.home){
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
